b6a47eb6d549abeb5c4eb72034c77a56d64466d6
bmurdock <brian.j.murdock@gmail.com>
2020-05-04 21:06:39 -0600

    update our character submission form

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch');
 
 // main home page route
-router.get('/', (req, res, next) => {
-    res.render('pages/index', {});
+router.get('/', async (req, res, next) => {
+    const characters = await fetch('http://localhost:3333/api/characters')
+        .then((response) => {
+            return response.json();
+        });
+    res.render('pages/index', {
+        characters
+    });
+});
+
+// add character page
+router.get('/add-character', async (req, res, next) => {
+    const classes = await fetch('http://localhost:3333/api/classes')
+        .then((response) => {
+            return response.json();
+        });
+
+    const races = await fetch('http://localhost:3333/api/races')
+        .then((response) => {
+            return response.json();
+        });
+
+    res.render('pages/add-character', {
+        classes,
+        races,
+    });
 });
 
 module.exports = router;
